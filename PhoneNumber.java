95d6dace67c2db4e4ac4b810edb5894496deb811
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-21 12:31:16 +0530

    validate phone number

--- PhoneNumber.java
+++ PhoneNumber.java
@@ -1,24 +1,21 @@
 package com.company1.Day19_UserRegistration;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Email {
-    // validating email
-    public int email(String str) {
+public class PhoneNumber {
+    public void validate(String str) {
         int flag = 0;
-        Pattern pattern = Pattern.compile("^abc.?[A-Za-z]?+@bl\\.co(\\.in)?$");
+        Pattern pattern = Pattern.compile("^(\\+?\\d{1,2}?)?(\\d{10})$");
         Matcher matcher = pattern.matcher(str);
         while (matcher.find()) {
             flag = 1;
         }
         if (flag == 1) {
-            System.out.println("valid email");
-            return 1;
+            System.out.println("valid phone number");
         } else {
-            System.out.println("invalid email");
-            return 0;
+            System.out.println("invalid phone number");
         }
     }
 }
-
--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -6,9 +6,9 @@ public class UserRegistrationMain {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Email Address : ");
-        String getEmail = sc.next();
-        Email emailId = new Email();
-        emailId.email(getEmail);
+        System.out.println("Enter Phone number : ");
+        String getUserInput = sc.next();
+        PhoneNumber phoneNumber = new PhoneNumber();
+        phoneNumber.validate(getUserInput);
     }
 }
